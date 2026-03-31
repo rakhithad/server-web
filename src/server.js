fc294a82505ccd5ad812b1ad31a8586df98abee3
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2026-04-01 03:46:55 +0530

    setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// src/server.js
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(helmet()); 
+app.use(cors()); 
+app.use(express.json());
+
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ status: 'success', message: 'API is running smoothly!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
