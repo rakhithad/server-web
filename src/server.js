6ed613fcd2ff6345999d87a0e014c13d6aaa7cea
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2026-04-28 21:42:08 +0530

    setup the frontend

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(helmet()); 
-app.use(cors()); 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
 
